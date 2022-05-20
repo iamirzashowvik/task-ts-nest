2c33a62c0418a5bf69ae424d5a301dd53026bfac
iamirzashowvik <showvikmirza@gmail.com>
2022-05-20 11:42:17 +0600

    make it better

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   const serverConfig = config.get('server');
-
-  const port = serverConfig.port || process.env.PORT;
+  //serverConfig.port ||
+  const port = process.env.PORT;
 
   console.log('Nest server listening on port  ', port);
   await app.listen(port);
