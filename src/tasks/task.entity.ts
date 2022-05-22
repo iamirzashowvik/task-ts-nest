584ef7bf045afc74fab9e26c16c755ec47c8dd88
iamirzashowvik <showvikmirza@gmail.com>
2022-05-22 11:30:41 +0600

    uuid

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,17 +1,21 @@
+import { isUUID } from 'class-validator';
 import { User } from 'src/auth/entities/user.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  Generated,
   ManyToOne,
+  PrimaryColumn,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 @Entity()
 export class Task extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn({ type: 'uuid' })
+  @Generated('uuid')
+  id: string;
 
   @Column()
   title: string;
